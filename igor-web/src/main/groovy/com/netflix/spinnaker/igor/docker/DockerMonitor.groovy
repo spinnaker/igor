spinnaker/spinnaker

commit 72ab6b723fbefe756863aff7ac0a497a4f9fae51

feat(docker): Fallback value for item circuit breaker in Docker poller (#255)

igor-web/src/main/groovy/com/netflix/spinnaker/igor/docker/DockerMonitor.groovy
@@ -20,6 +20,7 @@ import com.netflix.discovery.DiscoveryClient
 import com.netflix.spectator.api.Registry
 import com.netflix.spinnaker.igor.IgorConfigurationProperties
 import com.netflix.spinnaker.igor.build.model.GenericArtifact
+import com.netflix.spinnaker.igor.config.DockerRegistryProperties
 import com.netflix.spinnaker.igor.docker.model.DockerRegistryAccounts
 import com.netflix.spinnaker.igor.docker.service.TaggedImage
 import com.netflix.spinnaker.igor.history.EchoService
@@ -29,20 +30,21 @@ import com.netflix.spinnaker.igor.polling.DeltaItem
 import com.netflix.spinnaker.igor.polling.PollContext
 import com.netflix.spinnaker.igor.polling.PollingDelta
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.stereotype.Service
 
 import static net.logstash.logback.argument.StructuredArguments.kv
 
 @Service
 @SuppressWarnings('CatchException')
-@ConditionalOnProperty('dockerRegistry.enabled')
+@ConditionalOnClass(DockerRegistryAccounts)
 class DockerMonitor extends CommonPollingMonitor<ImageDelta, DockerPollingDelta> {
 
     private final DockerRegistryCache cache
     private final DockerRegistryAccounts dockerRegistryAccounts
     private final Optional<EchoService> echoService
-    private final Optional<DockerRegistryCacheV2KeysMigration> keysMigration;
+    private final Optional<DockerRegistryCacheV2KeysMigration> keysMigration
+    private final DockerRegistryProperties dockerRegistryProperties
 
     @Autowired
     DockerMonitor(IgorConfigurationProperties properties,
@@ -51,12 +53,14 @@ class DockerMonitor extends CommonPollingMonitor<ImageDelta, DockerPollingDelta>
                   DockerRegistryCache cache,
                   DockerRegistryAccounts dockerRegistryAccounts,
                   Optional<EchoService> echoService,
-                  Optional<DockerRegistryCacheV2KeysMigration> keysMigration) {
+                  Optional<DockerRegistryCacheV2KeysMigration> keysMigration,
+                  DockerRegistryProperties dockerRegistryProperties) {
         super(properties, registry, discoveryClient)
         this.cache = cache
         this.dockerRegistryAccounts = dockerRegistryAccounts
         this.echoService = echoService
-        this.keysMigration = keysMigration;
+        this.keysMigration = keysMigration
+        this.dockerRegistryProperties = dockerRegistryProperties
     }
 
     @Override
@@ -177,7 +181,11 @@ class DockerMonitor extends CommonPollingMonitor<ImageDelta, DockerPollingDelta>
 
     @Override
     protected Integer getPartitionUpperThreshold(String partition) {
-        return dockerRegistryAccounts.accounts.find { it.name == partition }?.itemUpperThreshold
+        def upperThreshold = dockerRegistryAccounts.accounts.find { it.name == partition }?.itemUpperThreshold
+        if (!upperThreshold) {
+            upperThreshold = dockerRegistryProperties.itemUpperThreshold
+        }
+        return upperThreshold
     }
 
     private static class DockerPollingDelta implements PollingDelta<ImageDelta> {

igor-web/src/main/groovy/com/netflix/spinnaker/igor/config/DockerRegistryConfig.groovy
@@ -23,6 +23,7 @@ import com.netflix.spinnaker.igor.docker.service.ClouddriverService
 import com.netflix.spinnaker.retrofit.Slf4jRetrofitLogger
 import groovy.transform.CompileStatic
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import retrofit.Endpoints
@@ -32,6 +33,7 @@ import retrofit.client.OkClient
 
 @Configuration
 @ConditionalOnProperty(['services.clouddriver.baseUrl', 'dockerRegistry.enabled'])
+@EnableConfigurationProperties(DockerRegistryProperties)
 @CompileStatic
 class DockerRegistryConfig {
     @Bean

igor-web/src/main/groovy/com/netflix/spinnaker/igor/config/DockerRegistryProperties.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright 2018 Schibsted ASA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.igor.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "dockerRegistry")
+public class DockerRegistryProperties {
+    private boolean enabled;
+    private Integer itemUpperThreshold;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Integer getItemUpperThreshold() {
+        return itemUpperThreshold;
+    }
+
+    public void setItemUpperThreshold(Integer itemUpperThreshold) {
+        this.itemUpperThreshold = itemUpperThreshold;
+    }
+}

igor-web/src/test/groovy/com/netflix/spinnaker/igor/docker/DockerMonitorSpec.groovy
@@ -19,6 +19,7 @@ package com.netflix.spinnaker.igor.docker
 
 import com.netflix.spectator.api.NoopRegistry
 import com.netflix.spinnaker.igor.IgorConfigurationProperties
+import com.netflix.spinnaker.igor.config.DockerRegistryProperties
 import com.netflix.spinnaker.igor.docker.model.DockerRegistryAccounts
 import com.netflix.spinnaker.igor.docker.service.TaggedImage
 import com.netflix.spinnaker.igor.history.EchoService
@@ -34,6 +35,7 @@ class DockerMonitorSpec extends Specification {
     def dockerRegistryCache = Mock(DockerRegistryCache)
     def dockerRegistryAccounts = Mock(DockerRegistryAccounts)
     def echoService = Mock(EchoService)
+    def dockerRegistryProperties = new DockerRegistryProperties(enabled: true, itemUpperThreshold: 5)
 
     @Unroll
     void 'should only publish events if account has been indexed previously'() {
@@ -47,7 +49,7 @@ class DockerMonitorSpec extends Specification {
         )
 
         when:
-        new DockerMonitor(properties, registry, discoveryClient, dockerRegistryCache, dockerRegistryAccounts, Optional.of(echoService), Optional.empty())
+        new DockerMonitor(properties, registry, discoveryClient, dockerRegistryCache, dockerRegistryAccounts, Optional.of(echoService), Optional.empty(), dockerRegistryProperties)
             .postEvent(cachedImages, taggedImage, "imageId")
 
         then:
@@ -61,7 +63,7 @@ class DockerMonitorSpec extends Specification {
         })
 
         when: "should short circuit if `echoService` is not available"
-        new DockerMonitor(properties, registry, discoveryClient, dockerRegistryCache, dockerRegistryAccounts, Optional.empty(), Optional.empty())
+        new DockerMonitor(properties, registry, discoveryClient, dockerRegistryCache, dockerRegistryAccounts, Optional.empty(), Optional.empty(), dockerRegistryProperties)
             .postEvent(["imageId"], taggedImage, "imageId")
 
         then:
@@ -86,7 +88,7 @@ class DockerMonitorSpec extends Specification {
         )
 
         when:
-        new DockerMonitor(properties, registry, discoveryClient, dockerRegistryCache, dockerRegistryAccounts, Optional.of(echoService), Optional.empty())
+        new DockerMonitor(properties, registry, discoveryClient, dockerRegistryCache, dockerRegistryAccounts, Optional.of(echoService), Optional.empty(), dockerRegistryProperties)
             .postEvent(["job1"], taggedImage, "imageId")
 
         then:
@@ -122,14 +124,40 @@ class DockerMonitorSpec extends Specification {
         new TaggedImage(tag: "tag", account: "account", registry: "registry", repository: "repository", digest: "digest")  | false       || false
         new TaggedImage(tag: "new", account: "account", registry: "registry", repository: "repository", digest: "digest")  | false       || true
         new TaggedImage(tag: "tag", account: "account", registry: "registry", repository: "repository", digest: "digest2") | true        || true
+    }
 
+    @Unroll
+    def "should retrieve itemUpperThreshold #upperThreshold for #partition with fallback value #fallbackThreshold from igor properties"() {
+        given:
+        def subject = createSubject()
+        dockerRegistryProperties.setItemUpperThreshold(fallbackThreshold)
+
+        when:
+        def result = subject.getPartitionUpperThreshold(partition)
+
+        then:
+        1 * dockerRegistryAccounts.accounts >> [
+          [name: 'partition1', itemUpperThreshold: 10],
+          [name: 'partition2', itemUpperThreshold: 20],
+          [name: 'partition3']
+        ]
+        assert result == upperThreshold
+
+        where:
+        partition    | fallbackThreshold || upperThreshold
+        'partition1' | 100               || 10
+        'partition1' | null              || 10
+        'partition2' | 100               || 20
+        'partition3' | 100               || 100
+        'partition4' | 100               || 100
+        'partition4' | null              || null
     }
 
     private DockerMonitor createSubject(Optional<EchoService> echoService) {
-        return new DockerMonitor(properties, registry, discoveryClient, dockerRegistryCache, dockerRegistryAccounts, echoService, Optional.empty())
+        return new DockerMonitor(properties, registry, discoveryClient, dockerRegistryCache, dockerRegistryAccounts, echoService, Optional.empty(), dockerRegistryProperties)
     }
 
-    private String keyFromTaggedImage(TaggedImage taggedImage) {
+    private static String keyFromTaggedImage(TaggedImage taggedImage) {
         return new DockerRegistryV2Key("prefix", DockerRegistryCache.ID, taggedImage.account, taggedImage.registry, taggedImage.tag).toString()
     }
 }

spinnaker/igor

commit f836147c4597199369d972d4437395771c42e2b9

fix(docker): Fix igor startup with dockerRegistry enabled
#257 (that was supposed to fix a regression in #255) caused the Docker Monitor to never initialize.
Revert to using `ConditionalOnProperty` instead.

igor-web/src/main/groovy/com/netflix/spinnaker/igor/docker/DockerMonitor.groovy
@@ -30,14 +30,14 @@ import com.netflix.spinnaker.igor.polling.DeltaItem
 import com.netflix.spinnaker.igor.polling.PollContext
 import com.netflix.spinnaker.igor.polling.PollingDelta
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 
 import static net.logstash.logback.argument.StructuredArguments.kv
 
 @Service
 @SuppressWarnings('CatchException')
-@ConditionalOnBean(DockerRegistryAccounts)
+@ConditionalOnProperty(['services.clouddriver.baseUrl', 'dockerRegistry.enabled'])
 class DockerMonitor extends CommonPollingMonitor<ImageDelta, DockerPollingDelta> {
 
     private final DockerRegistryCache cache