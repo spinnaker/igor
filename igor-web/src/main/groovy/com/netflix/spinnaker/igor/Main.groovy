5c883b9f3616351eb6fa8319f153edec00c87a96
Rob Zienert <rzienert@netflix.com>
2019-02-25 14:42:20 -0800

    refactor(*): Initial Spring Boot 2 refactor

--- igor-web/src/main/groovy/com/netflix/spinnaker/igor/Main.groovy
+++ igor-web/src/main/groovy/com/netflix/spinnaker/igor/Main.groovy
@@ -21,7 +21,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import sun.net.InetAddressCachePolicy
