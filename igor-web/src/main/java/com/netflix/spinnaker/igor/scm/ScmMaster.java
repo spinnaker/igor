13e9cdfe4c273a5bf81e97514fa64d5b38e106fd
Luis Pollo <lpollo@netflix.com>
2019-12-18 08:49:22 -0800

    fix(pr): Remove redundant modifiers from interface

--- igor-web/src/main/java/com/netflix/spinnaker/igor/scm/ScmMaster.java
+++ igor-web/src/main/java/com/netflix/spinnaker/igor/scm/ScmMaster.java
@@ -20,11 +20,9 @@ import java.util.List;
 
 /** Abstracts underlying implementation details of each SCM system under a common interface. */
 public interface ScmMaster {
-  public static final String DEFAULT_GIT_REF = "refs/heads/master";
+  String DEFAULT_GIT_REF = "refs/heads/master";
 
-  public abstract List<String> listDirectory(
-      String projectKey, String repositorySlug, String path, String at);
+  List<String> listDirectory(String projectKey, String repositorySlug, String path, String at);
 
-  public abstract String getTextFileContents(
-      String projectKey, String repositorySlug, String path, String at);
+  String getTextFileContents(String projectKey, String repositorySlug, String path, String at);
 }
