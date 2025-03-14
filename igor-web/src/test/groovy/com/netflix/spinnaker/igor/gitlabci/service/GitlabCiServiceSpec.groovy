1e73e2f95b056f1a512c601718ecb871b88d1bc1
alice garcia <alice_garcia@homedepot.com>
2025-03-14 16:12:55 -0400

    refactor(retrofit2): add tests for retry behavior in GitlabCiService

--- igor-web/src/test/groovy/com/netflix/spinnaker/igor/gitlabci/service/GitlabCiServiceSpec.groovy
+++ igor-web/src/test/groovy/com/netflix/spinnaker/igor/gitlabci/service/GitlabCiServiceSpec.groovy
@@ -16,15 +16,20 @@
 package com.netflix.spinnaker.igor.gitlabci.service
 
 import com.netflix.spinnaker.fiat.model.resources.Permissions
-import com.netflix.spinnaker.igor.config.GitlabCiProperties
 import com.netflix.spinnaker.igor.config.GitlabCiProperties.GitlabCiHost
-import com.netflix.spinnaker.igor.gitlabci.client.GitlabApiCannedResponses
 import com.netflix.spinnaker.igor.gitlabci.client.GitlabCiClient
+import com.netflix.spinnaker.igor.gitlabci.client.model.Job
 import com.netflix.spinnaker.igor.gitlabci.client.model.Pipeline
 import com.netflix.spinnaker.igor.gitlabci.client.model.Project
+import com.netflix.spinnaker.igor.helpers.TestUtils
+import com.netflix.spinnaker.igor.jenkins.client.model.Build
+import com.netflix.spinnaker.kork.retrofit.exceptions.SpinnakerHttpException
+import okhttp3.MediaType
+import okhttp3.ResponseBody
 import retrofit2.mock.Calls
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class GitlabCiServiceSpec extends Specification {
     @Shared
@@ -94,4 +99,76 @@ class GitlabCiServiceSpec extends Specification {
 
         1 * client.getPipelineSummaries(_, _) >> Calls.response([])
     }
+
+  @Unroll
+  def "getBuildProperties retries on transient failure"() {
+    given:
+    final String PROJECT_ID = "13"
+    final int PIPELINE_ID = 3
+    final int JOB_ID = 1
+
+    def build = new Build(number: PIPELINE_ID, duration: 0L)
+    def testPipeline = new Pipeline(id: PIPELINE_ID)
+    def testJob = new Job(pipeline: testPipeline, id: JOB_ID)
+    def badGatewayError = TestUtils.makeSpinnakerHttpException("http://test.net", 502, ResponseBody.create("{}", MediaType.parse("application/json")))
+    def jobLog = ResponseBody.create("SPINNAKER_PROPERTY_TEST=1",MediaType.parse("application/text"))
+
+    when:
+    def properties = service.getBuildProperties(PROJECT_ID, build.genericBuild(PROJECT_ID), "test.properties")
+
+    then:
+    2 * client.getPipeline(PROJECT_ID, PIPELINE_ID) >> { throw badGatewayError } >> Calls.response(testPipeline)
+    1 * client.getJobs(PROJECT_ID, PIPELINE_ID) >> Calls.response([testJob])
+    1 * client.getBridges(PROJECT_ID, PIPELINE_ID) >> Calls.response([])
+    1 * client.getJobLog(PROJECT_ID, JOB_ID) >> Calls.response(jobLog)
+
+    properties == [test: "1"]
+  }
+
+  @Unroll
+  def "getBuildProperties retries on 404"() {
+    given:
+    final String PROJECT_ID = "13"
+    final int PIPELINE_ID = 3
+    final int JOB_ID = 1
+
+    def build = new Build(number: PIPELINE_ID, duration: 0L)
+    def testPipeline = new Pipeline(id: PIPELINE_ID)
+    def testJob = new Job(pipeline: testPipeline, id: JOB_ID)
+    def notFoundError = TestUtils.makeSpinnakerHttpException("http://test.net", 404, ResponseBody.create("not found", MediaType.parse("application/text")))
+    def jobLog = ResponseBody.create("SPINNAKER_PROPERTY_TEST=1",MediaType.parse("application/text"))
+
+    when:
+    def properties = service.getBuildProperties(PROJECT_ID, build.genericBuild(PROJECT_ID), "test.properties")
+
+    then:
+    2 * client.getPipeline(PROJECT_ID, PIPELINE_ID) >> { throw notFoundError } >> Calls.response(testPipeline)
+    1 * client.getJobs(PROJECT_ID, PIPELINE_ID) >> Calls.response([testJob])
+    1 * client.getBridges(PROJECT_ID, PIPELINE_ID) >> Calls.response([])
+    1 * client.getJobLog(PROJECT_ID, JOB_ID) >> Calls.response(jobLog)
+
+    properties == [test: "1"]
+  }
+
+  @Unroll
+  def "getBuildProperties does not retry on 400"() {
+    given:
+    final String PROJECT_ID = "13"
+    final int PIPELINE_ID = 3
+    final int JOB_ID = 1
+
+    def build = new Build(number: PIPELINE_ID, duration: 0L)
+    def badRequestError = TestUtils.makeSpinnakerHttpException("http://test.net", 400, ResponseBody.create("bad request", MediaType.parse("application/text")))
+
+    when:
+    service.getBuildProperties(PROJECT_ID, build.genericBuild(PROJECT_ID), "test.properties")
+
+    then:
+    1 * client.getPipeline(PROJECT_ID, PIPELINE_ID) >> { throw badRequestError }
+    0 * client.getJobs(PROJECT_ID, PIPELINE_ID)
+    0 * client.getBridges(PROJECT_ID, PIPELINE_ID)
+    0 * client.getJobLog(PROJECT_ID, JOB_ID)
+
+    thrown SpinnakerHttpException
+  }
 }
